7cf04c0553e02d2b454fb516c65c037099479f68
Yoshiki Fujikane <ffjlabo@gmail.com>
2021-05-21 17:27:24 +0900

    feat: implement for wire

---
+++ config/config.go
@@ -0,0 +1,9 @@
+package config
+
+var (
+	ModuleName = "github.com/ffjlabo/auto-wire"
+	UsecaseDir = "generated/usecase"
+	InfraRepo  = "generated/infra/repository"
+	DomainRepo = "generated/domain/repository"
+	DiDir      = "generated/app/di"
+)
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ffjlabo/auto-wire
+
+go 1.16
+
+require github.com/google/wire v0.5.0
